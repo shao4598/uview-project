450e9bf5f5848de4c50407b8415cbcd361225df4
chloe <wentingjiang@outlook.com>
2023-12-07 21:43:12 +0800

    ci: 初始化uview

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import './uni.promisify.adaptor'
+import uView from 'uview-ui'
+import clickOutside from './utils/clickOutside.js'
 
 Vue.config.productionTip = false
+Vue.use(uView)
+Vue.use(clickOutside)
 
 App.mpType = 'app'
 
